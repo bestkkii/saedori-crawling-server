6f19025cb70884a9d1930f24a79b5750376362dd
c5mp <dohunhaha@gmail.com>
2025-04-22 16:31:45 +0900

    채은공주님 코드 반영

--- app/api/v1/routes.py
+++ app/api/v1/routes.py
@@ -1,10 +1,18 @@
 # Gin에서 크롤링 요청 받기 /api/????
 ## 크롤링 수행하기
-## DB에 저장
+## 데이터를 json 형식으로 변환
+## 데이터를 Gin에 전달
 
-# Gin에서 데이터 POST 받기 API 명세서에 따라 /api/v1/...
-## DB에서 데이터 조회
-## Gin에 전달
+# from fastapi import APIRouter
+
+# router = APIRouter()
+
+# @router.post("/crawl")
+# async def crawl_data(request: CrawlRequest):
+#     # 크롤링 수행
+#     # 데이터를 json 형식으로 변환
+#     # 데이터를 Gin에 전달
+#     return {"message": "Crawling completed"}
 
 from fastapi import APIRouter
 from app.services.data_service import music_service
